faf7ad3d1956f5c47958a539ea087d91018d6c7d
abelardogg <abelardogy@gmail.com>
2019-09-08 16:21:36 -0600

    service worker

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,5 @@
 const createEl = React.createElement;
 
-(function(){
-    console.log(`Start pocket Universe!`);
-
-    if ('serviceWorker' in navigator) {
-        window.addEventListener('load', function() {
-          navigator.serviceWorker.register('/sw.js').then(function(registration) {
-            // Registration was successful
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-          }, function(err) {
-            // registration failed :(
-            console.log('ServiceWorker registration failed: ', err);
-          });
-        });
-      }
-
+(function () {
+  console.log(`Start pocket Universe!`);
 }());
